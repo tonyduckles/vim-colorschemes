694491c2225ed774137298929396569937e646c6
Tony Duckles <tony@nynim.org>
2018-01-28 08:47:31 -0600

    pull-updates.sh: Use local cached Git checkouts to fetch updates

--- pull-updates.sh
+++ pull-updates.sh
@@ -2,24 +2,24 @@
 
 function fetch () {
     local repo="$1"
-    local branch="$2"
-    if [[ $branch == "" ]]; then
-        branch="master"
-    fi
     local tmp="tmp"
-    test -d "$tmp" || mkdir "$tmp"
+    test -d "$tmp" || mkdir -p "$tmp"
     echo ""
     echo "Fetching '${repo}' ..."
-    local sha=$(curl -s "https://api.github.com/repos/${repo}/branches/${branch}" | \
-                grep \"sha\" | cut -d'"' -f4 | head -n1)
-    echo "Extracting '${repo}'@${sha:0:7} ..."
-    curl -# -L "https://api.github.com/repos/${repo}/tarball/${branch}" | \
-        tar xz --strip=1 -C "${tmp}/"
-    find "${tmp}" -type f | xargs chmod 644
-    rsync -av "${tmp}/" --include='autoload/***' --include='colors/***' --exclude='*' .
-    rm -rf "$tmp"
+    local wc="${tmp}/${repo/\//_}"
+    # clone remote repo if doesn't exist in local cache directory
+    test -d "$wc" || git clone "https://github.com/${repo}.git" "$wc"
+    # pull any updates from the remote repo
+    git -C "$wc" pull
+    # get commit id for HEAD
+    local sha=$(git -C "$wc" rev-parse HEAD)
+    # copy relevant files to the root repo
+    rsync -a "${wc}/" --include='autoload/***' --include='colors/***' --exclude='*' .
+    # disable all executable bits
+    find "autoload" "colors" -type f | xargs chmod 644
+    # commit any changes
     git add "colors/*" "autoload/*" > /dev/null
-    git commit -m "Update '${repo}'@${sha:0:7}" > /dev/null
+    git commit -m "Update '${repo}' ${sha:0:7}"
 }
 
 fetch 'ajmwagar/vim-deus'
@@ -28,7 +28,7 @@ fetch 'altercation/vim-colors-solarized'
 fetch 'jacoborus/tender.vim'
 fetch 'jnurmine/Zenburn'
 fetch 'junegunn/seoul256.vim'
-fetch 'Lokaltog/vim-distinguished' develop
+fetch 'Lokaltog/vim-distinguished'
 fetch 'mhartington/oceanic-next'
 fetch 'morhetz/gruvbox'
 fetch 'rakr/vim-two-firewatch'
